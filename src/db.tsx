280f7fae9af9e219796b234f3a37a9246417b0cf
Borwornpob Thumrongchotikhun <borwornpob@gmail.com>
2024-05-02 14:28:26 +0700

    Init Lucia

--- src/db.tsx
+++ src/db.tsx
@@ -1,9 +1,29 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
-import postgres from 'postgres';
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
+import { user, session } from "./schema";
+import { Lucia } from "lucia";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL must be defined');
+  throw new Error("DATABASE_URL must be defined");
 }
 
 const queryClient = postgres(process.env.DATABASE_URL);
 export const db = drizzle(queryClient);
+
+// Init lucia
+const adapter = new DrizzlePostgreSQLAdapter(db, session, user);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    attributes: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+});
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof Lucia;
+  }
+}
--- src/schema.ts
+++ src/schema.ts
@@ -1,8 +1,33 @@
-import { serial, text, pgTable, pgSchema } from "drizzle-orm/pg-core";
+import {
+  serial,
+  text,
+  pgTable,
+  pgSchema,
+  timestamp,
+} from "drizzle-orm/pg-core";
 
 export const schema = pgSchema("note");
 
 export const user = pgTable("user", {
-  id: serial("id").primaryKey(),
-  name: text("name"),
+  id: text("id").primaryKey(),
+  profileId: text("id")
+    .unique()
+    .references(() => profile.id),
+});
+
+export const profile = pgTable("profile", {
+  id: text("id").primaryKey(),
+  firstName: text("first_name"),
+  lastName: text("last_name"),
+});
+
+export const session = pgTable("session", {
+  id: text("id").primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id),
+  expiresAt: timestamp("expires_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull(),
 });
